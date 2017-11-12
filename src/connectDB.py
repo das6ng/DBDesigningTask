8ec878db3f7571e0fe02fe398429467829a6f3be
root <root@localhost.localdomain>
2017-11-12 09:42:57 -0500

    db test demo.

---
+++ src/connectDB.py
@@ -0,0 +1,28 @@
+#!/bin/baaash/env python
+# -*- coding:utf-8 -*-
+
+# Usage:
+#     connect the Database
+#   DB name: StuMgr
+#
+
+import pymysql
+
+host = "127.0.0.1"
+port = 3306
+user = "stumgr"
+passwd = "1234"
+db = "StuMgr_test"
+
+def connectDB():
+    try:
+        conn = pymysql.connect(host=host, port=port, user=user, passwd=passwd, db=db, charset='utf8')
+        print "->DB connect sucess!"
+    except Exception,e:
+        print "->DB connect Failed!"
+    
+    return conn
+
+
+if __name__ == "__main__":
+    connectDB()
---
+++ src/initDB.py
@@ -0,0 +1,72 @@
+#!/bin/baaash/env python
+# -*- coding:utf-8 -*-
+
+# Usage:
+#     initialize the Database
+#
+
+import pymysql
+
+from connectDB import connectDB
+
+class initObj(object):
+    
+    def __init__(self):
+        self.DBfd = connectDB()
+        self.cursor = self.DBfd.cursor()
+        print " <initObj>connecter called"
+        
+    def __del__(self):
+        self.DBfd.close()
+        print "->Db connection close."
+
+    def dropTable(self, table):    
+        sql = "drop table %s;"
+        msg = self.cursor.execute(sql, table)
+        
+        print "msg D> ",msg
+        return msg
+        
+    def getAllData(self, table):
+        sql = "select * from test;"
+        msg = self.cursor.execute(sql)
+        
+        data = self.cursor.fetchall()
+        return data
+        
+    
+    def createTestTable(self):
+        msg = self.cursor.execute("create table test(id char(9) not null primary key, name char(16) not null);")
+        
+        print "msg C> ",msg
+        return msg
+    
+    def insertTestData(self, id_, name_):
+        sql = "insert into test(id, name) values (%s, %s);"
+        msg = self.cursor.execute(sql, (id_, name_))
+        
+        print "msg I> ",msg
+        return msg
+
+if __name__ == "__main__":
+    c = initObj()
+    try:
+        c.dropTable('test')
+    except Exception,e:
+        print "->drop exception"
+        pass
+    try:
+        c.createTestTable()
+    except Exception,e:
+        print "->create exception"
+        pass
+    for i in range(0,15):
+        c.insertTestData(str(i), 'n'+str(i))
+    try:
+        r = c.getAllData("test")
+        print "id name"
+        for i in r:
+            print i[0],' ',i[1]
+    except Exception,e:
+        print "->select exception. msg:",e
+        pass
