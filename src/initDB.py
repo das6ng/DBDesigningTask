4f085cf011ec99998c3e86df527f6b620fc87f74
dash-1 <dashengyeah@hotmail.com>
2017-11-15 16:38:20 +0800

    drag main sql cmds out to ./src/sqlcmd.py

--- src/initDB.py
+++ src/initDB.py
@@ -8,100 +8,7 @@
 import pymysql
 
 from connectDB import connectDB
-
-sql_dept = """
-create table tbl_department(
-    id char(2) not null primary key,
-    name char(36) not null,
-
-    index ID(id)
-);
-"""
-sql_course = """
-create table tbl_course(
-    id char(4) not null primary key,
-    name char(36) not null,
-
-    index ID(id)
-);
-"""
-sql_class = """
-create table tbl_class(
-    id char(2) not null primary key,
-    dept char(36) not null,
-    size smallint check (size > -1 and size < 255),
-
-    index ID(id),
-    foreign key (dept)
-        references tbl_department(id)
-        on update cascade
-);
-"""
-sql_teacher = """
-create table tbl_teacher(
-    id char(8) not null primary key,
-    name char(36) not null,
-    gender char(1) not null check (gender in ('F','M', 'X')),
-    dept char(36) not null,
-
-    index ID(id),
-    foreign key (dept)
-        references tbl_department(id)
-        on update cascade
-);
-"""
-sql_student = """
-create table tbl_student(
-    id char(11) not null primary key,
-    name char(36) not null,
-    gender char(1) not null check (gender in ('F','M', 'X')),
-    birthday char(8),
-    dept char(2),
-
-    index ID(id),
-    foreign key (dept)
-        references tbl_department(id)
-        on update cascade
-        on delete set null
-);
-"""
-sql_plan = """
-create table tbl_teachingplan(
-    class char(2) not null,
-    course char(4) not null,
-    dept char(2) not null,
-    semester smallint check(semester > 0 and semester < 9),
-    nature  char(10) check (nature in ('elective','compulsory')),
-    weight smallint check (weight > 0),
-
-    primary key (class, course, dept),
-    foreign key (class)
-        references tbl_class(id)
-        on update cascade,
-    foreign key (course)
-        references tbl_course(id)
-        on update cascade,
-    foreign key (dept)
-        references tbl_department(id)
-        on update cascade
-);
-"""
-sql_choice = """
-create table tbl_coursechoice(
-    id char(11) not null,
-    course char(4) not null,
-    grades smallint check(grades > -1 and grades < 101),
-    regrades smallint check(grades > -1 and grades < 101),
-
-    primary key (id, course),
-    foreign key (id)
-        references tbl_student(id)
-        on update cascade,
-    foreign key (course)
-        references tbl_course(id)
-        on update cascade
-);
-"""
+from sqlcmd import *
 
 class initObj(object):
 
@@ -118,15 +25,13 @@ class initObj(object):
 
     # ddrop a table
     def dropTable(self, table):
-        sql = "drop table %s;"%table
-        msg = self.cursor.execute(sql)
+        msg = self.cursor.execute(sql_drop_table % table)
         print "msg> ",sql
         return msg
 
     # get all data from a table
     def getAllData(self, table):
-        sql = "select * from %s;"%table
-        msg = self.cursor.execute(sql)
+        msg = self.cursor.execute(sql_select_all % table)
         print "msg> ",sql
 
         data = self.cursor.fetchall()
---
+++ src/sqlcmd.py
@@ -0,0 +1,113 @@
+#!/bin/baaash/env python
+# -*- coding:utf-8 -*-
+
+# Usage:
+#     All sql commands
+#   DB name: StuMgr
+#
+
+### common sql commands ###
+sql_drop_table = "drop table %s;"
+sql_select_all = "select * from %s;"
+
+### table creation ###
+# department table
+sql_dept = """
+create table tbl_department(
+    id char(2) not null primary key,
+    name char(36) not null,
+
+    index ID(id)
+);
+"""
+# course table
+sql_course = """
+create table tbl_course(
+    id char(4) not null primary key,
+    name char(36) not null,
+
+    index ID(id)
+);
+"""
+# class table
+sql_class = """
+create table tbl_class(
+    id char(2) not null primary key,
+    dept char(36) not null,
+    size smallint check (size > -1 and size < 255),
+
+    index ID(id),
+    foreign key (dept)
+        references tbl_department(id)
+        on update cascade
+);
+"""
+# teacher table
+sql_teacher = """
+create table tbl_teacher(
+    id char(8) not null primary key,
+    name char(36) not null,
+    gender char(1) not null check (gender in ('F','M', 'X')),
+    dept char(36) not null,
+
+    index ID(id),
+    foreign key (dept)
+        references tbl_department(id)
+        on update cascade
+);
+"""
+# student table
+sql_student = """
+create table tbl_student(
+    id char(11) not null primary key,
+    name char(36) not null,
+    gender char(1) not null check (gender in ('F','M', 'X')),
+    birthday char(8),
+    dept char(2),
+
+    index ID(id),
+    foreign key (dept)
+        references tbl_department(id)
+        on update cascade
+        on delete set null
+);
+"""
+# teaching plan table
+sql_plan = """
+create table tbl_teachingplan(
+    class char(2) not null,
+    course char(4) not null,
+    dept char(2) not null,
+    semester smallint check(semester > 0 and semester < 9),
+    nature  char(10) check (nature in ('elective','compulsory')),
+    weight smallint check (weight > 0),
+
+    primary key (class, course, dept),
+    foreign key (class)
+        references tbl_class(id)
+        on update cascade,
+    foreign key (course)
+        references tbl_course(id)
+        on update cascade,
+    foreign key (dept)
+        references tbl_department(id)
+        on update cascade
+);
+"""
+# course choice table
+sql_choice = """
+create table tbl_coursechoice(
+    id char(11) not null,
+    course char(4) not null,
+    grades smallint check(grades > -1 and grades < 101),
+    regrades smallint check(grades > -1 and grades < 101),
+
+    primary key (id, course),
+    foreign key (id)
+        references tbl_student(id)
+        on update cascade,
+    foreign key (course)
+        references tbl_course(id)
+        on update cascade
+);
+"""
